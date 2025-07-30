2ec55a2bb3390c023027764fc40c92daf95169f1
Kanika-subramanyam <kanika96885@gmail.com>
2025-07-30 21:16:17 +0530

    Add files via upload

---
+++ prodigy/script.js
@@ -0,0 +1,50 @@
+const cells = document.querySelectorAll(".cell");
+const statusText = document.getElementById("status");
+
+let currentPlayer = "X";
+let board = ["", "", "", "", "", "", "", "", ""];
+let gameActive = true;
+
+const winPatterns = [
+  [0,1,2], [3,4,5], [6,7,8], 
+  [0,3,6], [1,4,7], [2,5,8], 
+  [0,4,8], [2,4,6]           
+];
+
+cells.forEach(cell => {
+  cell.addEventListener("click", () => handleClick(cell));
+});
+
+function handleClick(cell) {
+  const index = cell.getAttribute("data-index");
+
+  if (board[index] !== "" || !gameActive) return;
+
+  board[index] = currentPlayer;
+  cell.textContent = currentPlayer;
+
+  if (checkWin()) {
+    statusText.textContent =` Player ${currentPlayer} wins!`;
+    gameActive = false;
+  } else if (board.every(cell => cell !== "")) {
+    statusText.textContent = "It's a draw!";
+    gameActive = false;
+  } else {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusText.textContent = `Player ${currentPlayer}'s turn`;
+  }
+}
+
+function checkWin() {
+  return winPatterns.some(pattern => {
+    return pattern.every(index => board[index] === currentPlayer);
+  });
+}
+
+function resetGame() {
+  board = ["", "", "", "", "", "", "", "", ""];
+  gameActive = true;
+  currentPlayer = "X";
+  statusText.textContent = "Player X's turn";
+  cells.forEach(cell => cell.textContent = "");
+}
